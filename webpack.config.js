582a92c5ee9eecb7b587cd78f5728bf1ce3d2f03
peaBerberian <pea.berberian@gmail.com>
2019-01-16 16:42:40 +0100

    update dependencies and version

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,15 @@
 /* eslint-env node */
 const path = require("path");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-
-const shouldMinify = process.env.AISOBMFFWVDFBUTFAII_MINIFY;
-
+const shouldMinify = !!process.env.AISOBMFFWVDFBUTFAII_MINIFY;
 const outputFilename = shouldMinify ?
   "aisobmffwvdfbutfaii.min.js" : "aisobmffwvdfbutfaii.js";
 
 const config = {
-  mode: "production",
+  mode: shouldMinify ? "production" : "development",
   entry: path.join(__dirname, "./src/index.js"),
   output: {
     path: path.join(__dirname, "/build"),
     filename: outputFilename,
-  },
-  optimization: {
-    minimizer: shouldMinify ? [new UglifyJsPlugin()] : [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          compress: {
-            keep_infinity: true,
-            inline: false,
-            reduce_funcs: false, // does not work well on commentated funcs.
-                                 // TODO open issue on uglify
-          },
-          keep_fnames: true,
-          keep_classnames: true,
-          keep_fargs: true,
-          mangle: false,
-          output: {
-            beautify: true,
-            comments: true,
-          },
-        },
-      }),
-    ],
   },
 };
 
